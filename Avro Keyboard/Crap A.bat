e076abaf18d0f93762d4f46c6d8f6b6f701c69d2
Shakil Shahadat <shakil.regi@gmail.com>
2021-04-13 22:07:46 +0600

    Create Crap A.bat

---
+++ Avro Keyboard/Crap A.bat
@@ -0,0 +1,18 @@
+REM Tested for version 5.5.0 & 5.6.0
+
+rmdir /Q/S "C:\Program Files (x86)\Avro Keyboard\images"
+del "C:\Program Files (x86)\Avro Keyboard\Avro Keyboard.url"
+del "C:\Program Files (x86)\Avro Keyboard\Bangla Typing with Avro Mouse.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\Bangla Typing with Avro Phonetic.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\Bangla Typing with Fixed Keyboard Layouts.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\Before You Start.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\Configuring_system.htm"
+del "C:\Program Files (x86)\Avro Keyboard\Customizing Avro Keyboard.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\Editing Keyboard Layout.pdf"
+del "C:\Program Files (x86)\Avro Keyboard\help_on_help.htm"
+del "C:\Program Files (x86)\Avro Keyboard\MPL-1.1.txt"
+del "C:\Program Files (x86)\Avro Keyboard\open_type_font_list.htm"
+del "C:\Program Files (x86)\Avro Keyboard\Overview.pdf"
+
+REM Only for 5.5.0
+del "C:\Program Files (x86)\Avro Keyboard\Support forum.url"
